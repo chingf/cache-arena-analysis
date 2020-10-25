9436697970c92bcbea22fc8b4040bae1b16a4ce1
Ching <chingfang17@gmail.com>
2020-10-25 14:17:16 -0400

    Refactor code for new data structure convention with checks

--- analysis/ExpData.py
+++ analysis/ExpData.py
@@ -7,9 +7,9 @@ class ExpData(object):
     formatting and add additional useful data structures.
 
     Attrs:
-        x_loc: (frames,) array; x location of bird position
-        y_loc: (frames,) array; y location of bird position
-        theta: (frames,) array; angle of bird position relative to center
+        x: (frames,) array; x location of bird position
+        y: (frames,) array; y location of bird position
+        thetas: (frames,) array; angle of bird position relative to center
         wedges: (frames,) array; which wedge the bird is on
         visit_s: (neurs, visits) array; average spikes each visit
         visit_durs: (visits,) array; frame duration of each visit
@@ -18,12 +18,13 @@ class ExpData(object):
         visit_wedges: (visits,) array; wedge location of each visit
         visit_was_retrieval: (visits,) array; whether visit was retrieval
         visit_was_cache: (visits,) array; whether visit was cache
-        cr_sites: (cr,) array: location of each cache/retrieval event
-        cr_pokes: (cr,) array: frame of each cache/retrieval poke
-        cr_enters: (cr,) array: frame of hop into cache/retrieval event
-        cr_exits: (cr,) array: frame of hop out of cache/retrieval event
-        cr_was_retrieval: (cr,) array; whether or not cr event was retrieval
-        cr_was_cache: (cr,) array; whether or not cr event was cache
+        event_sites: (events,) array: location of each cache/retrieval event
+        event_pokes: (events,) array: frame of each cache/retrieval poke
+        event_enters: (events,) array: frame of hop into cache/retrieval event
+        event_exits: (events,) array: frame of hop out of cache/retrieval event
+        cache_event: (events,) array; whether event was cache
+        retriev_event: (events,) array; whether event was retrieval
+        check_event: (events,) array; whether event was check
         spikes: (neurs, frames) array; number of spikes of each neur per frame
 
 
@@ -33,33 +34,46 @@ class ExpData(object):
 
     def __init__(self, f):
         if 'XDLC' in f.keys() and 'YDLC' in f.keys():
-            self.x_loc = np.array(f['XDLC']['Body']).squeeze()
-            self.y_loc = np.array(f['YDLC']['Body']).squeeze()
+            self.x = np.array(f['XDLC']['Body']).squeeze()
+            self.y = np.array(f['YDLC']['Body']).squeeze()
         else:
-            self.x_loc = np.array(f['X']).squeeze()
-            self.y_loc = np.array(f['Y']).squeeze()
-        self.theta = np.array(f['theta']).squeeze()
+            self.x = np.array(f['X']).squeeze()
+            self.y = np.array(f['Y']).squeeze()
+        self.thetas = np.array(f['theta']).squeeze()
         self.wedges = np.array(f['whichWedge']).squeeze()
         self.visit_s = np.array(f['VS']).squeeze().T
         self.visit_durs = np.array(f['VisitDur']).squeeze().astype(int)
         self.visit_enters = np.array(f['VisitStart']).squeeze().astype(int) - 1
         self.visit_exits = self.visit_enters + self.visit_durs
         self.visit_wedges = np.array(f['VisitWedge']).squeeze().astype(int)
-        self.cr_sites = np.array(f['CacheSites']).squeeze().astype(int)
-        self.cr_pokes = np.array(f['CacheFrames']).squeeze().astype(int) - 1
-        self.cr_enters = np.array(f['CacheFramesEnter']).squeeze().astype(int) - 1
-        self.cr_exits = np.array(f['CacheFramesExit']).squeeze().astype(int)
-        self.cr_was_retrieval = np.array(f['ThisWasRetrieval']).squeeze().astype(bool)
-        self.cr_was_cache = np.logical_not(self.cr_was_retrieval)
-        self._remove_repeated_crs()
-        self._sort_crs()
+
+        if "LMN" in f.filename:
+            self.event_sites = np.array(f['EventSites']).squeeze().astype(int)
+            self.event_pokes = np.array(f['EventFrames']).squeeze().astype(int) - 1
+            self.event_enters = np.array(f['EventFramesEnter']).squeeze().astype(int) - 1
+            self.event_exits = np.array(f['EventFramesExit']).squeeze().astype(int)
+            event_labels = np.array(f['TypeOfEvent']).squeeze()
+            self.cache_event = (event_labels == 1)
+            self.retriev_event = (event_labels == 2)
+            self.check_event = (event_labels == 3)
+        else:
+            self.event_sites = np.array(f['CacheSites']).squeeze().astype(int)
+            self.event_pokes = np.array(f['CacheFrames']).squeeze().astype(int) - 1
+            self.event_enters = np.array(f['CacheFramesEnter']).squeeze().astype(int) - 1
+            self.event_exits = np.array(f['CacheFramesExit']).squeeze().astype(int)
+            self.retriev_event = np.array(f['ThisWasRetrieval']).squeeze().astype(bool)
+            self.cache_event = np.logical_not(self.retriev_event)
+            self.check_event = np.zeros(event_sites.size).astype(bool)
+
+        self._remove_repeated_events()
+        self._sort_events()
         self.spikes = np.array(f['S']).T
         self.num_neurs, self.num_frames = self.spikes.shape
         self.fr = get_fr(self.spikes)
 
     def get_cr_visits(self):
         """
-        Labels each visit as cache/retrieval, or non-cr.
+        Labels each visit as cache/retrieval, or non-cache/retrieval.
 
         Returns:
             cr_visits, noncr_visits: arrays that index into visits to extract
@@ -67,12 +81,18 @@ class ExpData(object):
             should be (visits,)
         """
 
-        _, cr_visits, _ = np.intersect1d(
-            self.visit_enters, self.cr_enters, return_indices = True
+        _, c_visits, _ = np.intersect1d(
+            self.visit_enters, self.event_enters[self.cache_event],
+            return_indices = True
             )
+        _, r_visits, _ = np.intersect1d(
+            self.visit_enters, self.event_enters[self.retriev_event],
+            return_indices = True
+            )
+        cr_visits = np.concatenate((c_visits, r_visits))
         noncr_visits = np.arange(self.visit_enters.size)
         noncr_visits = np.setdiff1d(noncr_visits, cr_visits)
-        return cr_visits, noncr_visits
+        return c_visits, r_visits, noncr_visits
 
     def get_hopcentered_visits(self, window):
         """
@@ -91,27 +111,29 @@ class ExpData(object):
         hopcentered_visits[hopcentered_visits >= self.num_frames] = -1
         return hopcentered_visits
 
-    def _remove_repeated_crs(self):
+    def _remove_repeated_events(self):
         unique_enters = []
-        keep_cr = np.ones(self.cr_enters.size).astype(bool)
-        for idx, enter in enumerate(self.cr_enters):
+        keep_event = np.ones(self.event_enters.size).astype(bool)
+        for idx, enter in enumerate(self.event_enters):
             if enter in unique_enters:
-                keep_cr[idx] = False
+                keep_event[idx] = False
             else:
                 unique_enters.append(enter)
-        self.cr_sites = self.cr_sites[keep_cr]
-        self.cr_pokes = self.cr_pokes[keep_cr]
-        self.cr_enters = self.cr_enters[keep_cr]
-        self.cr_exits = self.cr_exits[keep_cr]
-        self.cr_was_retrieval = self.cr_was_retrieval[keep_cr]
-        self.cr_was_cache = self.cr_was_cache[keep_cr]
+        self.event_sites = self.event_sites[keep_event]
+        self.event_pokes = self.event_pokes[keep_event]
+        self.event_enters = self.event_enters[keep_event]
+        self.event_exits = self.event_exits[keep_event]
+        self.cache_event = self.cache_event[keep_event]
+        self.retriev_event = self.retriev_event[keep_event]
+        self.check_event = self.check_event[keep_event]
 
-    def _sort_crs(self):
-        sorting = np.argsort(self.cr_enters)
-        self.cr_sites = self.cr_sites[sorting]
-        self.cr_pokes = self.cr_pokes[sorting]
-        self.cr_enters = self.cr_enters[sorting]
-        self.cr_exits = self.cr_exits[sorting]
-        self.cr_was_retrieval = self.cr_was_retrieval[sorting]
-        self.cr_was_cache = self.cr_was_cache[sorting]
+    def _sort_events(self):
+        sorting = np.argsort(self.event_enters)
+        self.event_sites = self.event_sites[sorting]
+        self.event_pokes = self.event_pokes[sorting]
+        self.event_enters = self.event_enters[sorting]
+        self.event_exits = self.event_exits[sorting]
+        self.cache_event = self.cache_event[sorting]
+        self.retriev_event = self.retriev_event[sorting]
+        self.check_event = self.check_event[sorting]
 
--- analysis/config.py
+++ analysis/config.py
@@ -8,10 +8,13 @@ behav_data_dir = engram_dir / 'BehavioralData'
 birds = ['RBY45', 'LMN73']
 
 # Collects filepaths to data
-auto_collect = False # Directory structure is not yet consistent for auto-collect
+auto_collect = True # Directory structure is not yet consistent for auto-collect
 h5_path_dict = {}
+h5_path_dict['RBY45'] = [
+    f for f in (neural_data_dir / 'ForChing').iterdir() if f.suffix == '.mat'
+    ]
 if auto_collect:
-    for bird in birds:
+    for bird in ['LMN73']:
         h5_path_dict[bird] = []
         bird_dir = neural_data_dir / bird
         for date_dir in [d for d in bird_dir.iterdir() if d.is_dir()]:
@@ -20,19 +23,17 @@ if auto_collect:
                 if not results_dir.exists(): continue
                 extracted_files = [
                     f for f in results_dir.iterdir() if f.stem.startswith(
-                        "ExtractedWithXY"
+                        "ExtractedWithDLCAndAnnotations"
                         )
                     ]
                 if len(extracted_files) == 0: continue
                 extracted_files.sort()
                 h5_path_dict[bird].append(extracted_files[-1])
 else:
-    h5_path_dict['RBY45'] = [
-        f for f in (neural_data_dir / 'ForChing').iterdir() if f.suffix == '.mat'
-        ]
     lmn_dir = neural_data_dir / 'LMN73'
     h5_path_dict['LMN73'] = [
         lmn_dir / '20200908/2020-09-08-12-02-54_video_SplitIntoBatches_29_250_10_200/Results/ExtractedWithXY_Cleaned182657_09182020.mat',
-        lmn_dir / '20200910/2020-09-10-12-00-48_video_SplitIntoBatches_29_250_10_200/Results/ExtractedWithXY_Cleaned181248_09182020.mat'
+        lmn_dir / '20200910/2020-09-10-12-00-48_video_SplitIntoBatches_29_250_10_200/Results/ExtractedWithXY_Cleaned181248_09182020.mat',
+        lmn_dir / '20200918/2020-09-18-11-50-00_video_SplitIntoBatches_29_250_10_200/Results/ExtractedWithDLCAndAnnotations153159_10222020.mat'
         ]
 
